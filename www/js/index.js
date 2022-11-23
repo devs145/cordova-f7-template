72dafc9de8de7b55fd573ff9a7f626df8367c0c7
devs145 <sahildevnani145@gmail.com>
2022-11-23 11:46:10 -0700

    updates

--- www/js/index.js
+++ www/js/index.js
@@ -16,41 +16,65 @@ var app = new Framework7({
 var mainView = app.views.create('.view-main')
 
 var $$ = Dom7;
+var lat;
+var long;
+var map;
+var marker;
+var geoOpts = {
+    enableHighAccuracy: true 
+ }
+var watchID;
+
+
 $$(document).on('page:init', '.page[data-name="page2"]', function () {
     // Page 2 fun here
+     map = new google.maps.Map(document.getElementById('map'),{
+        zoom: 18,
+        center: {lat:lat, lng: long}    
+    }) 
+     marker = new google.maps.Marker({
+        position: {lat:lat, lng:long},
+        map: map
+    })
 
+    $("#startWatch").on('click', function() {
+        watchID= navigator.geolocation.watchPosition(watchSuccess, geoError, geoOpts)
+        $(this).hide()
+        $("#stopWatch").show()
+    })
+
+    $("#stopWatch").on('click', function() {
+        navigator.geolocation.clearWatch(watchID)
+        $(this).hide()
+        $("#startWatch").show()
+    })
+    
+function watchSuccess(position){
+    console.log(position);
+     lat = position.coords.latitude
+     long = position.coords.longitude
+
+     var coords = {lat:lat, lng:long}
+     map.setCenter(coords);
+     marker.setPosition(coords);
+     
+//      $("#currentPos").append("<p>" + lat + "," + long + "</p>")
+}
 })
 
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-
-    var geoOpts = {
-       enableHighAccuracy: true 
-    }
     navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOpts);
+}
 
-    function geoSuccess(position){
-        console.log(position);
-        var lat = position.coords.latitude
-        var long = position.coords.longitude
-        $("#currentPos").html(lat + "," + long)
-    }
-    function geoError(message) {
-        alert(message.message);
-    }
-    var watchID;
-
-    $("#startWatch").on('click', function() {
-        watchID= navigator.geolocation.watchPosition(geoSuccess, geoError, geoOpts)
-        $(this).hide()
-        $("#stopWatch").show()
-    })
-    $("#stopWatch").on('click', function() {
-        navigator.geolocation.clearWatch(watchID)
-        $(this).hide()
-        $("#startWatch").show()
-    })
-
+function geoSuccess(position){
+    console.log(position);
+     lat = position.coords.latitude
+     long = position.coords.longitude
+     $("#currentPos").append("<p>" + lat + "," + long + "</p>")
+}
+function geoError(message) {
+    alert(message.message);
 }
